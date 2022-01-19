075d16c4c8c40e8adf5d0dd1900d079800ec3134
Miao-Long <myau.christopher@gmail.com>
2022-01-18 18:40:30 -0800

    Adds data and refactors some logic

---
+++ stat_data.py
@@ -0,0 +1,223 @@
+hp_circlet_sub = {
+    "HP_f":15.00,
+    "AK_f":15.00,
+    "DF_f":15.00,
+    "HP_p":0,
+    "AK_p":10.00,
+    "DF_p":10.00,
+    "ER_p":10.00,
+    "EM_f":10.00,
+    "CR_p":7.50,
+    "CD_p":7.50
+}
+
+ak_circlet_sub = {
+    "HP_f":15.00,
+    "AK_f":15.00,
+    "DF_f":15.00,
+    "HP_p":10.00,
+    "AK_p":0,
+    "DF_p":10.00,
+    "ER_p":10.00,
+    "EM_f":10.00,
+    "CR_p":7.50,
+    "CD_p":7.50
+}
+
+df_circlet_sub = {
+    "HP_f":15.00,
+    "AK_f":15.00,
+    "DF_f":15.00,
+    "HP_p":10.00,
+    "AK_p":10.00,
+    "DF_p":0,
+    "ER_p":10.00,
+    "EM_f":10.00,
+    "CR_p":7.50,
+    "CD_p":7.50
+}
+
+em_circlet_sub = {
+    "HP_f":15.00,
+    "AK_f":15.00,
+    "DF_f":15.00,
+    "HP_p":10.00,
+    "AK_p":10.00,
+    "DF_p":10.00,
+    "ER_p":10.00,
+    "EM_f":0,
+    "CR_p":7.50,
+    "CD_p":7.50
+}
+
+cr_circlet_sub = {
+    "HP_f":14.63,
+    "AK_f":14.63,
+    "DF_f":14.63,
+    "HP_p":9.76,
+    "AK_p":9.76,
+    "DF_p":9.76,
+    "ER_p":9.76,
+    "EM_f":9.76,
+    "CR_p":0,
+    "CD_p":7.32
+}
+
+cr_circlet_sub = {
+    "HP_f":14.63,
+    "AK_f":14.63,
+    "DF_f":14.63,
+    "HP_p":9.76,
+    "AK_p":9.76,
+    "DF_p":9.76,
+    "ER_p":9.76,
+    "EM_f":9.76,
+    "CR_p":7.32,
+    "CD_p":0
+}
+
+hb_circlet_sub = {
+    "HP_f":13.64,
+    "AK_f":13.64,
+    "DF_p":13.64,
+    "HP_p":9.09,
+    "AK_p":9.09,
+    "DF_p":9.09,
+    "ER_p":9.09,
+    "EM_f":9.09,
+    "CR_p":6.82,
+    "CD_p":6.82
+}
+
+#TODO write in the rest of the stats
+circlet_stats_dict = {
+    "HP_p":(22.00, hp_circlet_sub),
+    "AK_p":(22.00, ak_circlet_sub),
+    "DF_p":(22.00, df_circlet_sub),
+    "CR_p":(10.00, cr_circlet_sub),
+    "CD_p":(10.00, cd_circlet_sub),
+    "HB_p":(10.00, hb_circlet_sub),
+    "EM_p":(4.00, en_circlet_sub)
+}
+
+hp_timepiece_sub = {
+    "HP_f":15.00,
+    "AK_f":15.00,
+    "DF_f":15.00,
+    "HP_p":0,
+    "AK_p":10.00,
+    "DF_p":10.00,
+    "ER_p":10.00,
+    "EM_f":10.00,
+    "CR_p":7.50,
+    "CD_p":7.50
+}
+
+ak_timepiece_sub = {
+    "HP_f":15.00,
+    "AK_f":15.00,
+    "DF_f":15.00,
+    "HP_p":10.00,
+    "AK_p":0,
+    "DF_p":10.00,
+    "ER_p":10.00,
+    "EM_f":10.00,
+    "CR_p":7.50,
+    "CD_p":7.50
+}
+
+df_timepiece_sub = {
+    "HP_f":15.00,
+    "AK_f":15.00,
+    "DF_f":15.00,
+    "HP_p":10.00,
+    "AK_p":10.00,
+    "DF_p":0,
+    "ER_p":10.00,
+    "EM_f":10.00,
+    "CR_p":7.50,
+    "CD_p":7.50
+}
+
+er_timepiece_sub = {
+    "HP_f":15.00,
+    "AK_f":15.00,
+    "DF_f":15.00,
+    "HP_p":10.00,
+    "AK_p":10.00,
+    "DF_p":10.00,
+    "ER_p":0,
+    "EM_f":10.00,
+    "CR_p":7.50,
+    "CD_p":7.50
+}
+
+em_timepiece_sub = {
+    "HP_f":15.00,
+    "AK_f":15.00,
+    "DF_f":15.00,
+    "HP_p":10.00,
+    "AK_p":10.00,
+    "DF_p":10.00,
+    "ER_p":10.00,
+    "EM_f":0,
+    "CR_p":7.50,
+    "CD_p":7.50
+}
+
+goblet_sub = {
+    "HP_f":15.79,
+    "AK_f":0,
+    "DF_f":15.79,
+    "HP_p":10.53,
+    "AK_p":10.53,
+    "DF_p":10.53,
+    "ER_p":10.53,
+    "EM_f":10.53,
+    "CR_p":7.50,
+    "CD_p":7.50
+}
+
+flower_sub = {
+    "HP_f":0,
+    "AK_f":15.79,
+    "DF_f":15.79,
+    "HP_p":10.53,
+    "AK_p":10.53,
+    "DF_p":10.53,
+    "ER_p":10.53,
+    "EM_f":10.53,
+    "CR_p":7.89,
+    "CD_p":7.88
+}
+
+feather_sub = {
+    "HP_f":15.79,
+    "AK_f":0,
+    "DF_f":15.79,
+    "HP_p":10.53,
+    "AK_p":10.53,
+    "DF_p":10.53,
+    "ER_p":10.53,
+    "EM_f":10.53,
+    "CR_p":7.89,
+    "CD_p":7.88
+}
+
+substat_dist = {
+    "HP_f": [209.13, 239.00, 268.88, 298.75],
+    "AK_f": [13.62, 15.56, 17.51, 19.45],
+    "DF_f": [16.20, 18.52, 20.83, 23.15],
+    "HP_p": [4.08, 4.66, 5.25, 5.83],
+    "AK_p": [4.08, 4.66, 5.25, 5.83],
+    "DF_p": [5.10, 5.83, 6.56, 7.29],
+    "ER_p": [4.53, 5.18, 5.83, 6.48],
+    "CR_p": [2.72, 3.11, 3.50, 3.89],
+    "CD_p": [5.44, 6.22, 6.99, 7.77]
+}
+
+main_circlet = pd.DataFrame.from_dict({'attr':["HP_p", "AK_p", "DF_p", "CR_p", "CD_p", "HB_p", "EM_f"], 'chnc':[22.00,22.00,22.00,10.00,10.00,10.00,4.00]})
+main_timepiece = pd.DataFrame.from_dict({'attr':["HP_p", "AK_p", "DF_p", "ER_p", "EM_f"], 'chnc':[26.68,26.66,26.66,10.00,10.00]})
+main_goblet = pd.DataFrame.from_dict({'attr':["HP_p", "AK_p", "DF_p", "PY_p", "EL_p","CY_p", "HY_p","AN_p", "GE_p","PH_p", "EM_f"], 'chnc':[21.25,21.25,20.00,5.00,5.00,5.00,5.00,5.00,5.00,5.00,2.50]})
+main_flower = pd.DataFrame.from_dict({'attr':["HP_f"], 'chnc':[100]})
+main_feather = pd.DataFrame.from_dict({'attr':["AK_f"], 'chnc':[100]})
