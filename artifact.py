e54866813747e829efe7077d938f5e383a715de4
Miao-Long <myau.christopher@gmail.com>
2022-01-29 21:48:45 -0800

    Update artifact.py
    added comprehension

--- artifact.py
+++ artifact.py
@@ -8,8 +8,8 @@ from urllib.request import urlretrieve
 from os.path import exists
 
 debug = True
-
 profile_focus = ["EM_f"]
+indexes = [0,1,2,3]
 
 def generate_artifact():
     artifact, stat = assign_mainstat()
@@ -20,16 +20,24 @@ def generate_artifact():
 
 def roll_substats(substat_dict):
     num_rolls = random.choices([3,4], weights=(75, 25), k=1)[0] + 5
-    indexes = [0,1,2,3]
-    substat_distribution = [stat_data.substat_dist[key] for key in np.random.choice(list(substat_dict.keys()), 4, replace=False, p=[x/100 for x in list(substat_dict.values())])]
-    substat_rolls = random.choices(indexes, k=num_rolls)
+    substat_names = np.random.choice(list(substat_dict.keys()), 4, replace=False, p=[x/100 for x in list(substat_dict.values())])
+    substat_distribution = [stat_data.substat_dist[key] for key in substat_names]
+    substat_rolls = random.choices(substat_names, k=num_rolls)
     substat_power = random.choices(indexes, k=num_rolls)
+    
+        
+    artifact_dict = {key:0 for key in substat_names}
+    for substat, power in zip(substat_rolls, substat_power):
+        artifact_dict[substat] += stat_data.substat_dist[substat][power]
     if debug:
         print(substat_distribution)
         print(substat_rolls)
         print(substat_power)
+        print(artifact_dict)
+
     #TODO next list comprehension to add substats
     #TODO check matrix sums
+    #TODO each stat gets at least 1 hit
     return True
 
 def assign_mainstat():
