dbd5ef0d2944118a21ff9ef545dd2fefd23e0c10
Miao-Long <myau.christopher@gmail.com>
2022-02-03 16:43:20 -0800

    PEP 8 style

--- artifact.py
+++ artifact.py
@@ -1,7 +1,5 @@
 import sys
 import random
-import datetime
-import pandas as pd
 import numpy as np
 import stat_data
 from os.path import exists
@@ -26,24 +24,37 @@ class Artifact:
 
     def _roll_substats(self, input_substat_dict):
         #set up parameters, normalize input data to 1
-        data_weights = [float(x)/sum(list(input_substat_dict.values())) for x in list(input_substat_dict.values())]
-        data_keys = list(input_substat_dict.keys()) 
+        data_weights = [
+            float(x)/sum(input_substat_dict.values()) 
+            for x in input_substat_dict.values()
+        ]
+        data_keys = list(input_substat_dict.keys())
         num_rolls = random.choices([3,4], weights=(75, 25), k=1)[0]
 
         #roll for substat values
         substat_names = np.random.choice(data_keys, 4, replace=False, p=data_weights)
         substat_rolls = random.choices(substat_names, k=num_rolls + 5)
-        substat_power = list(np.random.choice([0,1,2,3], 4, replace=False)) + \
-                        random.choices([0,1,2,3], k=num_rolls+1)
+        substat_power = (
+            list(np.random.choice([0,1,2,3], 4, replace=False)) +
+            random.choices([0,1,2,3], k=num_rolls+1)
+        )
 
         #write artifact and output
-        artifact_dict = {key:0 for key in substat_names}
+        artifact_dict = dict.fromkeys(substat_names, 0)
         for substat, power in zip(substat_rolls, substat_power):
             artifact_dict[substat] += stat_data.substat_dist[substat][power]
 
-        return {key:round(artifact_dict[key]) for key in artifact_dict.keys()}
+        return {key: round(value) for key, value in artifact_dict.items()}
 
     def _assign_mainstat(self):
+        #choose mainstat
         main_type = random.choice(list(stat_data.artifact_type.items()))
-        return (main_type[0], np.random.choice(main_type[1]['attr'], 1, replace=False, p = [x/100 for x in main_type[1]['chnc']])[0])
+
+        #normalize mainstat weights
+        main_weights = [
+            x/sum(main_type[1]['chnc'])
+            for x in main_type[1]['chnc']
+        ]
+
+        return (main_type[0], np.random.choice(main_type[1]['attr'], 1, replace=False, p = main_weights)[0])
 
