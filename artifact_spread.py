f121edd433312e7bd594187188fbfc19fb30d353
Miao-Long <myau.christopher@gmail.com>
2022-02-10 20:11:22 -0800

    Write to csv file done

--- artifact_spread.py
+++ artifact_spread.py
@@ -12,26 +12,11 @@ def eval_artifact(input_artifact, eval_profile):
     #return the sum of all of the substat values within the input profile, multiplied by the profile's weight for that substat
     return sum([value * eval_profile[key] if key in eval_profile else 0 for key, value in input_artifact.get_substats().items()])
 
-#TODO change string format to just returning a list
 def artifact_to_csv(a):
     substat_dict = dict.fromkeys(substat_names, 0)
-    for key in a.get_substats():
-        substat_dict[key] = a.get_substats()[key]
+    substat_dict.update(a.get_substats())
 
-    return "{type},{mainstat},{hp_f},{ak_f},{df_f},{hp_p},{ak_p},{df_p},{er_p},{em_f},{cr_p},{cd_p}".format(
-        type=a.get_type(),
-        mainstat=a.get_mainstat(),
-        hp_f=substat_dict["HP_f"],
-        ak_f=substat_dict["AK_f"],
-        df_f=substat_dict["DF_f"],
-        hp_p=substat_dict["HP_p"],
-        ak_p=substat_dict["AK_p"],
-        df_p=substat_dict["DF_p"],
-        er_p=substat_dict["ER_p"],
-        em_f=substat_dict["EM_f"],
-        cr_p=substat_dict["CR_p"],
-        cd_p=substat_dict["CD_p"]
-    )
+    return [a.get_type(), a.get_mainstat()] + list(substat_dict.values())
 
 def generative_model():
     a = artifact.Artifact()
@@ -53,12 +38,11 @@ def generative_model():
 
 def main():
     with open('artifact_data.csv', 'w', newline='') as csvfile:
-        artist = csv.writer(csvfile, delimiter=' ', quotechar="'")
-        a = generative_model()[0]
-        a_txt = artifact_to_csv(a)
-        print(a)
-        print(a_txt)
-        artist.writerow([a_txt])
+        artist = csv.writer(csvfile, delimiter=',', quotechar="'")
+        (a, b, i) = generative_model()
+        a_row = artifact_to_csv(a)
+        b_row = artifact_to_csv(b)
+        artist.writerow([i] + a_row + b_row)
 
 
 
