29bf4b4c8abccb49f93f50d24bbbc736f1697e30
Miao-Long <myau.christopher@gmail.com>
2022-02-12 14:51:21 -0800

    added structure to electro crit profile

--- artifact_spread.py
+++ artifact_spread.py
@@ -1,9 +1,24 @@
 import sys
 import csv
 import artifact
+import datetime
 
 substat_names = ["HP_f","AK_f","DF_f","HP_p","AK_p","DF_p","ER_p","EM_f","CR_p","CD_p"]
-CRIT_PROFILE = {"CR_p":4, "CD_p":2, "AK_p":1}
+ELECTRO_CRIT_PROFILE = {
+    "mainstats":{
+        "Circlet":["CR_p", "CD_p"], 
+        "Timepiece":["AK_p"], 
+        "Goblet":["EL_p"], 
+        "Feather":["AK_f"], 
+        "Flower":["HP_f"]
+            }, 
+    "substats":{
+        "CR_p":4, 
+        "CD_p":2, 
+        "AK_p":1
+    }
+}
+
 MAX_TESTS = 1000
 TOTAL_RUNS = 1000
 DEBUG = False
@@ -11,10 +26,13 @@ ARTIFACT_TYPE = "Circlet"
 
 def eval_artifact(input_artifact, eval_profile):
     #TODO need to tweak scoring algorithm for circlets
+
     #return the sum of all of the substat values within the input profile, multiplied by the profile's weight for that substat
     return sum([value * eval_profile[key] if key in eval_profile else 0 for key, value in input_artifact.get_substats().items()])
 
 def artifact_to_csv(a):
+    if a is None:
+        return []
     substat_dict = dict.fromkeys(substat_names, 0)
     substat_dict.update(a.get_substats())
 
@@ -22,13 +40,15 @@ def artifact_to_csv(a):
 
 def generative_model():
     a = artifact.Artifact(ARTIFACT_TYPE)
-    for i in range( MAX_TESTS):
+    for i in range(MAX_TESTS):
         b = artifact.Artifact()
-        if a.get_type() == b.get_type():
-            if eval_artifact(a, CRIT_PROFILE) < eval_artifact(b, CRIT_PROFILE):
+        if (a.get_type() == b.get_type() and
+            b.get_mainstat() in ELECTRO_CRIT_PROFILE["mainstats"][ARTIFACT_TYPE]): 
+            if eval_artifact(a, ELECTRO_CRIT_PROFILE["substats"]) < eval_artifact(b, ELECTRO_CRIT_PROFILE["substats"]): #what if I do this but factoring in mainstat into the scoring?
                 break
-    if i == MAX_TESTS:
+    if i == MAX_TESTS - 1:
         i = None
+        b = None
     if DEBUG:
         if i == MAX_TESTS:
             print("None Found")
@@ -39,7 +59,9 @@ def generative_model():
     return (a, b, i)
 
 def main():
-    with open('artifact_data.csv', 'w', newline='') as csvfile:
+    begin = datetime.datetime.now()
+    j = 0
+    with open(ARTIFACT_TYPE + '_artifact_data.csv', 'w', newline='') as csvfile:
         artist = csv.writer(csvfile, delimiter=',', quotechar="'")
         artist.writerow([
             "NumRuns",
@@ -70,6 +92,7 @@ def main():
         for i in range(TOTAL_RUNS):
             (a, b, i) = generative_model()
             artist.writerow([i] + artifact_to_csv(a) + artifact_to_csv(b))
+    print(datetime.datetime.now() - begin)
 
 
 
